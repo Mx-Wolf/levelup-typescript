e76ccc1940c2390f8803dfacba38b04b429675ac
Maxim Volkov <mx_ruz_net@hotmail.com>
2021-12-04 21:58:51 +0300

    location changed

--- projects-ts/logbook/src/index.ts
+++ projects-ts/logbook/src/index.ts
@@ -6,39 +6,51 @@ import { createHeader } from './views/header.js';
 import { createMain } from './views/main.js';
 import { createPivot } from './views/pivot.js';
 
+let rerender:(()=>void) | null = null;
 
-const collectChildren = (context:AppContext<RowData>)=>{
-  const {getState}= context;
-  const {location, rows} = getState();
-  return [
-    createHeader({location,setLocation:()=>undefined}),
-    location==='logbook'?createMain({
-      location,
-      setTitle:(title)=>{document.title=title;},
-      columns,
-      rows,
-    }):false,
-    location==='pivot-table'? createPivot():false,
-  ].filter((e)=>e) as HTMLElement[];
-};
-const run =(root:HTMLDivElement|null)=>{
-  if(root === null){
+const app = createApp<RowData>({
+  columnLabeler: 'key in functions catalog to show labels on top',
+  comparer: 'key in functions catalog to compare rows for grouping',
+  location: 'logbook',
+  message: undefined,
+  rowCount: 0,
+  rowLabeler: 'key in function catalog to show labels on the left',
+  rows: createMockRowData(),
+  rowsState: 'ready',
+});
+const handleLocationChange = ()=>{
+  if(rerender === null){
     return;
   }
-  const data = createMockRowData();
-  const app = createApp<RowData>({
-    columnLabeler:'key in functions catalog to show labels on top',
-    comparer: 'key in functions catalog to compare rows for grouping',
-    location: 'logbook',
-    message: undefined,
-    rowCount: data.length,
-    rowLabeler: 'key in function catalog to show labels on the left',
-    rows: data,
-    rowsState: 'ready',
-  });
+  rerender();
+};
+app.locationChanged.subscribe(handleLocationChange);
+const root = document.querySelector('div.wrapper') as HTMLDivElement;
+
+const collectChildren = (context: AppContext<RowData>) => {
+  const { getState, setLocation } = context;
+
+  const { location, rows, } = getState();
+  return [
+    createHeader({ location, setLocation }),
+    location === 'logbook' ? createMain({
+      location,
+      setTitle: (title) => { document.title = title; },
+      columns,
+      rows,
+    }) : false,
+    location === 'pivot-table' ? createPivot() : false,
+  ].filter((e) => e) as HTMLElement[];
+};
+const run = () => {
+  if (root === null) {
+    return;
+  }
+
   const children = collectChildren(app);
-  root.innerHTML='';
-  root.append(...children  );
+  root.innerHTML = '';
+  root.append(...children);
 };
 
-run(document.querySelector('div.wrapper') as HTMLDivElement);
+rerender = () => run();
+rerender();
