3ba68b19d5a3e236c77e9ae7c5b5a59f64af70f3
Maxim Volkov <mx_ruz_net@hotmail.com>
2021-12-08 02:20:42 +0300

    field heading h2

--- projects-ts/logbook/src/views/pivot-frame.ts
+++ projects-ts/logbook/src/views/pivot-frame.ts
@@ -1,11 +1,21 @@
 //import { pivot } from '../biz/pivot.js';
 import { PivotConfiguration } from '../models/app-state.js';
 import { RowData } from '../models/row-data.js';
+import { fieldLabels } from '../settings/labels.js';
 import { createHtmlElement, createText } from './parser.js';
 
 interface PivotFrameProps {
   rows: RowData[],
 }
+
+const createFieldHeading = (field: keyof typeof fieldLabels) => createHtmlElement(
+  'h2',
+  {
+    'class':'table-pivot__title'
+  },
+  [
+    createText(fieldLabels[field])
+  ]);
 
 export const  createPivotFrame = (props:PivotFrameProps & PivotConfiguration)=> {
   const {rows, rowGroup, aggregator,columnGroup}=props;
@@ -22,7 +32,7 @@ export const  createPivotFrame = (props:PivotFrameProps & PivotConfiguration)=> 
           'class':'table-pivot__values table-pivot__values--left'
         },
         [
-          createText(rowGroup.join(',')),
+          createFieldHeading(rowGroup[0] as keyof typeof fieldLabels),
         ]
       ),
       createHtmlElement(
@@ -31,6 +41,7 @@ export const  createPivotFrame = (props:PivotFrameProps & PivotConfiguration)=> 
           'class':'table-pivot__values table-pivot__values--left'
         },
         [
+          createFieldHeading(columnGroup[0] as keyof typeof fieldLabels),
           createText(columnGroup.join(',')),
           createText(aggregator.join(',')),
           createText(rows.length.toLocaleString()),
