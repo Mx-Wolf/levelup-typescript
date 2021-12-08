eaabdc79191b1a43f6b6dc1893d956165141e77b
Maxim Volkov <mx_ruz_net@hotmail.com>
2021-12-08 05:13:39 +0300

    pivot

--- projects-ts/logbook/src/views/pivot-frame.ts
+++ projects-ts/logbook/src/views/pivot-frame.ts
@@ -1,9 +1,11 @@
 //import { pivot } from '../biz/pivot.js';
 import { pivot } from '../biz/pivot.js';
-import { PivotConfiguration } from '../models/app-state.js';
+import { createTable } from '../components/table/table.js';
+import { ColumnSettings, PivotConfiguration } from '../models/app-state.js';
 import { RowData } from '../models/row-data.js';
 import { aggregateFunctions, groupingFunctions } from '../settings/grouping-functions.js';
 import { fieldLabels } from '../settings/labels.js';
+import { defineColumns } from '../utils/define-columns.js';
 import { createHtmlElement, createText } from './parser.js';
 
 interface PivotFrameProps {
@@ -24,6 +26,14 @@ const createDivTable = (children: Node[]) => createHtmlElement('div', { 'class':
 const getGroupingConfig = (functionName: string) => groupingFunctions[functionName as keyof typeof groupingFunctions];
 const getAggregateConfig = (functionName: string) => aggregateFunctions[functionName as keyof typeof aggregateFunctions];
 
+const prepareColumnDefinition = (order:number, label:string)=> <ColumnSettings<Record<string,string>>>{
+  format: (r)=>r[order],
+  label,
+  size: 'middle',
+  orderBy: undefined,
+  columnId: order,
+};
+
 export const createPivotFrame = (props: PivotFrameProps & PivotConfiguration<RowData>) => {
   const { rows, rowGroup, aggregator, columnGroup } = props;
   const rowConfig = getGroupingConfig(rowGroup[1]);
@@ -34,7 +44,8 @@ export const createPivotFrame = (props: PivotFrameProps & PivotConfiguration<Row
     rowConfig.createComparer(rowGroup[0]),
     columnConfig.createComparer(columnGroup[0]),
   );
-  const aggregateReducer = aggregate.createReducer(aggregator[0]);
+  const columnFormatter = columnConfig.createFormatter(columnGroup[0]);
+  const aggregateReducer = aggregate.createReducer<number>(aggregator[0]);
   const aggregated = pivotedResult.groups.map(
     (pivotedRow)=>pivotedRow.map(
       (pivotedColumn)=>pivotedColumn.reduce(aggregateReducer,aggregate.getInitialValue())));
@@ -53,7 +64,24 @@ export const createPivotFrame = (props: PivotFrameProps & PivotConfiguration<Row
         [
           createFieldHeading(rowGroup[0] as keyof typeof fieldLabels),
 
-          createDivTable([createText('labels go here')]),
+          createDivTable([
+            createTable({
+              columns:defineColumns([{
+                format:rowConfig.createFormatter(rowGroup[0]),
+                label: '',
+                size: 'middle',
+                orderBy: undefined,
+              }]),
+              dataRowCellClass: 'table__cell',
+              dataRowClass: 'table__row table__row--blue',
+              headerClass: 'table__header',
+              headerRowCellClass: 'table__cell table__cell--empty',
+              headerRowClass: 'table__row',
+              rows: pivotedResult.rowOrder.map((order)=> pivotedResult.rowIndex[order]),
+              tableClass: 'table__table',
+              tbodyClass: '',
+            }),
+          ]),
 
         ]
       ),
@@ -65,11 +93,20 @@ export const createPivotFrame = (props: PivotFrameProps & PivotConfiguration<Row
         [
           createFieldHeading(columnGroup[0] as keyof typeof fieldLabels),
           createDivTable([
-            createText('pivot goes here'),
-            createText(columnGroup.join(',')),
-            createText(aggregator.join(',')),
-            createText(rows.length.toLocaleString()),
-            createText(aggregated.length.toLocaleString()),
+            createTable<Record<string,string>>({
+              columns: pivotedResult.columnOrder.map((order)=>prepareColumnDefinition(order,columnFormatter(pivotedResult.columnIndex[order]))),
+              dataRowCellClass: 'table__cell',
+              dataRowClass: 'table__row',
+              headerClass: 'table__header table__header--blue',
+              headerRowCellClass: 'table__cell',
+              headerRowClass:'table__row',
+              rows: pivotedResult.rowOrder.map((rowOrder,ix)=>pivotedResult.columnOrder.reduce((r,columnOrder)=>{
+                r[`${ix}`] = `${aggregated[rowOrder][columnOrder]}`;
+                return r;
+              },{}as Record<string,string>)),
+              tableClass: 'table__table',
+              tbodyClass: '',
+            })
           ]),
 
         ]
