3563c2b86e85b8e844bb8541a8417e378989b974
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-01-08 14:06:47 +0300

    demo-05-13-01

--- modules/05-type-manipulation/13-dm-property-changed/01/code/types.ts
+++ modules/05-type-manipulation/13-dm-property-changed/01/code/types.ts
@@ -5,3 +5,28 @@ export interface PropertyChangedHandler<T> {
   // property - это имя свойства, которое изменилось
   (context: T, property: keyof T): void;
 }
+
+// Обозначим для себя тип действия 
+interface CancelSubscription {
+  (): void;
+}
+
+// Наша цель - для каждого "простого" свойства
+// простого объекта предоставить программе возможность
+// подписать процедуру для получения уведомления
+// Определим интерфейс для оформления и отказа 
+// от подсписки
+export interface PropertyChangedEvent<T>{
+  subscribe:(handler:PropertyChangedHandler<T>)=>CancelSubscription;
+  unsubscribe:(handler: PropertyChangedHandler<T>)=>void;
+}
+
+// Мы создадим код для генерации экземпляров 
+// менеджеров событий
+// Для каждого наблюдаемого свойства мы подготовим 
+// экземпляр с таким интерфейсом
+export interface EventManager<T> {
+  subscriptionManager: PropertyChangedEvent<T>;
+  dispatch: PropertyChangedHandler<T>
+}
+
